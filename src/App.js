7a91cfa1e1cc1175c3be2689a7fcfc0827a245da
gravityfliprunne <sarthakdhanotiya44@gmail.com>
2025-11-21 00:39:47 +0530

    Updated home page or fixed QR payment feature

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ function App() {
 
     const orderText = cart.map((c) => `${c.name} x${c.qty}`).join(", ");
 
-    fetch(" http://172.20.10.8:8080/api/sendOrder", {
+    fetch("http://172.20.10.8:8080/api/sendOrder", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
